44cc3ead5f0bfebb6127d13637d07391610a627c
salvavelazquez <augustorvelazquez@gmail.com>
2025-05-29 23:48:44 -0300

    Ajustar el espaciado vertical en mobiles para el login y register

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -23,125 +23,127 @@ const LoginView = () => {
   });
 
   return (
-    <div className="flex justify-center items-center mt-6 mb-5">
-      <div className="w-full md:max-w-md max-w-[350px] bg-gradient-to-br from-[#D3D4C4] to-[#B3B19C] p-6 rounded-[25px] shadow-[-20px_-20px_60px_#FFFFFF,10px_10px_30px_#778474] relative flex flex-col items-center">
-        <h2 className="text-[24px] font-semibold text-center text-[#444B3B] tracking-[0.005em]">Iniciar Sesión</h2>
+    <div className="flex flex-col min-h-[calc(100vh-120px)] justify-center">
+      <div className="flex justify-center items-center">
+        <div className="w-full md:max-w-md max-w-[350px] bg-gradient-to-br from-[#D3D4C4] to-[#B3B19C] p-6 rounded-[25px] shadow-[-20px_-20px_60px_#FFFFFF,10px_10px_30px_#778474] relative flex flex-col items-center">
+          <h2 className="text-[24px] font-semibold text-center text-[#444B3B] tracking-[0.005em]">Iniciar Sesión</h2>
 
-        <Formik
-          initialValues={{ email: "", password: "" }}
-          validationSchema={validationSchema}
-          onSubmit={async (values) => {
-            try {
-              const response = await login(values);
-              const { token, user } = response;
-              setUserData({ token, user });
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            validationSchema={validationSchema}
+            onSubmit={async (values) => {
+              try {
+                const response = await login(values);
+                const { token, user } = response;
+                setUserData({ token, user });
 
-              await Swal.fire({
-                icon: "success",
-                title: "Inicio de sesión exitoso",
-                text: "Bienvenido de nuevo!",
-              });
-              // Verificar si es administrador
-              if (user.role === 'admin') {
-                return router.push("/dashboard/admin");
-              }
+                await Swal.fire({
+                  icon: "success",
+                  title: "Inicio de sesión exitoso",
+                  text: "Bienvenido de nuevo!",
+                });
+                // Verificar si es administrador
+                if (user.role === 'admin') {
+                  return router.push("/dashboard/admin");
+                }
 
-              if (user.userProfile !== null) {
-                return router.push("/home");
-              }
+                if (user.userProfile !== null) {
+                  return router.push("/home");
+                }
 
 
-            } catch (error) {
-              if (error instanceof Error) {
-                if (error.message === "Tu cuenta está inactiva. Por favor, contacta al soporte.") {
-                  await Swal.fire({
-                    icon: "error",
-                    title: "Cuenta inactiva",
-                    text: error.message,
-                  });
+              } catch (error) {
+                if (error instanceof Error) {
+                  if (error.message === "Tu cuenta está inactiva. Por favor, contacta al soporte.") {
+                    await Swal.fire({
+                      icon: "error",
+                      title: "Cuenta inactiva",
+                      text: error.message,
+                    });
+                  } else {
+                    await Swal.fire({
+                      icon: "error",
+                      title: "Error de inicio de sesión",
+                      text: error.message,
+                    });
+                  }
                 } else {
                   await Swal.fire({
                     icon: "error",
                     title: "Error de inicio de sesión",
-                    text: error.message,
+                    text: "Ocurrió un error desconocido.",
                   });
                 }
-              } else {
-                await Swal.fire({
-                  icon: "error",
-                  title: "Error de inicio de sesión",
-                  text: "Ocurrió un error desconocido.",
-                });
               }
-            }
-          }}
-        >
-          {({ isSubmitting }) => (
-            <Form className="w-full md:max-w-[350px] space-y-4 md:space-y-6">
-              <div className="flex flex-col">
-                <label htmlFor="email" className="block text-lg font-medium text-gray-700">Email:</label>
-                <Field
-                  type="email"
-                  name="email"
-                  placeholder="johndoe@gmail.com"
-                  className="w-full h-[42px] bg-white shadow-[inset_2px_2px_7px_#000000] rounded-[25px] p-2 pl-4 text-[#313131] focus:outline-none"
-                />
-                <ErrorMessage name="email" component="span" className="text-red-500 text-sm mt-1" />
-              </div>
-
-              <div className="flex flex-col relative">
-                <label htmlFor="password" className="block text-lg font-medium text-gray-700">Contraseña:</label>
-                <div className="relative">
+            }}
+          >
+            {({ isSubmitting }) => (
+              <Form className="w-full md:max-w-[350px] space-y-4 md:space-y-6">
+                <div className="flex flex-col">
+                  <label htmlFor="email" className="block text-lg font-medium text-gray-700">Email:</label>
                   <Field
-                    type={showPassword ? "text" : "password"}
-                    name="password"
-                    placeholder="*******"
+                    type="email"
+                    name="email"
+                    placeholder="johndoe@gmail.com"
                     className="w-full h-[42px] bg-white shadow-[inset_2px_2px_7px_#000000] rounded-[25px] p-2 pl-4 text-[#313131] focus:outline-none"
                   />
-                  <button
-                    type="button"
-                    className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-700"
-                    onClick={() => setShowPassword(!showPassword)}
-                  >
-                    {showPassword ? <FaEyeSlash /> : <FaEye />}
-                  </button>
+                  <ErrorMessage name="email" component="span" className="text-red-500 text-sm mt-1" />
                 </div>
-                <ErrorMessage name="password" component="span" className="text-red-500 text-sm mt-1" />
-              </div>
 
-              <div className="flex items-center justify-center my-2">
-                <div className="flex-grow border-t-2 border-gray-500"></div>
-              </div>
-              <div className='flex ml-5 mt-5'>
-                <div className="relative flex justify-center items-center">
-                  <button
-                    type="submit"
-                    disabled={isSubmitting}
-                    className="w-[144px] left-[116px] bg-[#f78707] text-white py-2 px-4 rounded-xl disabled:opacity-50 transition-transform transform hover:scale-110"
-                  >
-                    {isSubmitting ? "Ingresando..." : "Ingresar"}
-                  </button>
+                <div className="flex flex-col relative">
+                  <label htmlFor="password" className="block text-lg font-medium text-gray-700">Contraseña:</label>
+                  <div className="relative">
+                    <Field
+                      type={showPassword ? "text" : "password"}
+                      name="password"
+                      placeholder="*******"
+                      className="w-full h-[42px] bg-white shadow-[inset_2px_2px_7px_#000000] rounded-[25px] p-2 pl-4 text-[#313131] focus:outline-none"
+                    />
+                    <button
+                      type="button"
+                      className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-700"
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {showPassword ? <FaEyeSlash /> : <FaEye />}
+                    </button>
+                  </div>
+                  <ErrorMessage name="password" component="span" className="text-red-500 text-sm mt-1" />
                 </div>
-                
-                <button
-                  className="flex items-center justify-center w-full h-[42px] transition-transform transform hover:scale-125"
-                  onClick={loginWithGoogle}
-                >
-                  <FaGoogle className="text-white-500 md:text-[44px]" /> oogle
-                </button>
 
-              </div>
-              <p className="text-xs text-center text-gray-500">
-                ¿No tienes una cuenta?{' '}
-                <a href="/register" className="font-medium text-[#eaefe4] hover:underline">
-                  Registrate aquí
-                </a>
-              </p>
-            </Form>
-          )}
-        </Formik>
+                <div className="flex items-center justify-center my-2">
+                  <div className="flex-grow border-t-2 border-gray-500"></div>
+                </div>
+                <div className='flex ml-5 mt-5'>
+                  <div className="relative flex justify-center items-center">
+                    <button
+                      type="submit"
+                      disabled={isSubmitting}
+                      className="w-[144px] left-[116px] bg-[#f78707] text-white py-2 px-4 rounded-xl disabled:opacity-50 transition-transform transform hover:scale-110"
+                    >
+                      {isSubmitting ? "Ingresando..." : "Ingresar"}
+                    </button>
+                  </div>
+
+                  <button
+                    className="flex items-center justify-center w-full h-[42px] transition-transform transform hover:scale-125"
+                    onClick={loginWithGoogle}
+                  >
+                    <FaGoogle className="text-white-500 md:text-[44px]" /> oogle
+                  </button>
+
+                </div>
+                <p className="text-xs text-center text-gray-500">
+                  ¿No tienes una cuenta?{' '}
+                  <a href="/register" className="font-medium text-[#eaefe4] hover:underline">
+                    Registrate aquí
+                  </a>
+                </p>
+              </Form>
+            )}
+          </Formik>
 
 
+        </div>
       </div>
     </div>
   );
--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -16,145 +16,146 @@ const RegisterView = () => {
     const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
     return (
+        <div className="flex flex-col min-h-[calc(100vh-20px)] md:min-h-[calc(100vh-60px)] justify-center">
+            <div className="flex justify-center items-center">
+                <div className="w-full md:max-w-md max-w-[350px] bg-gradient-to-br from-[#D3D4C4] to-[#B3B19C] p-6 rounded-[25px] shadow-[-20px_-20px_60px_#FFFFFF,10px_10px_30px_#778474] relative flex flex-col items-center">
+                    <h2 className="text-[24px] font-semibold text-center text-[#444B3B] tracking-[0.005em]">Registro</h2>
+                    <Formik
+                        initialValues={{
+                            name: '',
+                            email: '',
+                            password: '',
+                            passwordConfirmation: '',
+                        }}
 
-        <div className="flex justify-center items-center mb-12 mt-8">
-            <div className="w-full md:max-w-md max-w-[350px] bg-gradient-to-br from-[#D3D4C4] to-[#B3B19C] p-6 rounded-[25px] shadow-[-20px_-20px_60px_#FFFFFF,10px_10px_30px_#778474] relative flex flex-col items-center">
-                <h2 className="text-[24px] font-semibold text-center text-[#444B3B] tracking-[0.005em]">Registro</h2>
-                <Formik
-                    initialValues={{
-                        name: '',
-                        email: '',
-                        password: '',
-                        passwordConfirmation: '',
-                    }}
+                        onSubmit={async (values, { setSubmitting }) => {
+                            try {
+                                await register(values);
+                                await Swal.fire({
+                                    icon: "success",
+                                    title: "Registro exitoso",
+                                    text: "Ahora puedes iniciar sesión",
+                                });
+                                router.push('/login');
+                            } catch (error) {
+                                let errorMessage = "Credenciales incorrectas o error en el servidor.";
 
-                    onSubmit={async (values, { setSubmitting }) => {
-                        try {
-                            await register(values);
-                            await Swal.fire({
-                                icon: "success",
-                                title: "Registro exitoso",
-                                text: "Ahora puedes iniciar sesión",
-                            });
-                            router.push('/login');
-                        } catch (error) {
-                            let errorMessage = "Credenciales incorrectas o error en el servidor.";
+                                // Verificar si es un Error estándar
+                                if (error instanceof Error) {
+                                    errorMessage = error.message;
+                                }
+                                // Verificar si es una respuesta de error de la API
+                                else if (typeof error === 'object' && error !== null && 'message' in error) {
+                                    errorMessage = (error as { message: string }).message;
+                                }
 
-                            // Verificar si es un Error estándar
-                            if (error instanceof Error) {
-                                errorMessage = error.message;
+                                await Swal.fire({
+                                    icon: "error",
+                                    title: "Error en el registro",
+                                    text: errorMessage,
+                                });
+                            } finally {
+                                setSubmitting(false);
                             }
-                            // Verificar si es una respuesta de error de la API
-                            else if (typeof error === 'object' && error !== null && 'message' in error) {
-                                errorMessage = (error as { message: string }).message;
-                            }
+                        }}
 
-                            await Swal.fire({
-                                icon: "error",
-                                title: "Error en el registro",
-                                text: errorMessage,
-                            });
-                        } finally {
-                            setSubmitting(false);
-                        }
-                    }}
+                        validate={validateRegisterForm}
+                    >
+                        {({ errors, touched, isValid }) => (
 
-                    validate={validateRegisterForm}
-                >
-                    {({ errors, touched, isValid }) => (
+                            <Form className="w-full md:max-w-[350px] space-y-4 md:space-y-3">
 
-                        <Form className="w-full md:max-w-[350px] space-y-4 md:space-y-3">
+                                <div className="flex flex-col">
+                                    <label htmlFor="name" className="block text-lg font-medium text-gray-700">Nombre</label>
+                                    <Field type="text" name="name" id="name" className="w-full h-[42px] bg-white shadow-[inset_2px_2px_7px_#000000] rounded-[25px] p-2 pl-4 text-[#313131] focus:outline-none" placeholder="Jhon" required="" />
+                                    {errors.name && touched.name && (
+                                        <Alert type="error" message={errors.name} />
+                                    )}
+                                </div>
 
-                            <div className="flex flex-col">
-                                <label htmlFor="name" className="block text-lg font-medium text-gray-700">Nombre</label>
-                                <Field type="text" name="name" id="name" className="w-full h-[42px] bg-white shadow-[inset_2px_2px_7px_#000000] rounded-[25px] p-2 pl-4 text-[#313131] focus:outline-none" placeholder="Jhon" required="" />
-                                {errors.name && touched.name && (
-                                    <Alert type="error" message={errors.name} />
-                                )}
-                            </div>
+                                <div>
+                                    <label htmlFor="email" className="block text-lg font-medium text-gray-700">Email</label>
+                                    <Field type="email" name="email" id="email" className="w-full h-[42px] bg-white shadow-[inset_2px_2px_7px_#000000] rounded-[25px] p-2 pl-4 text-[#313131] focus:outline-none" placeholder="Jhon@gmail.com" required="" />
+                                    {errors.email && touched.email && (
+                                        <Alert type="error" message={errors.email} />
+                                    )}
+                                </div>
 
-                            <div>
-                                <label htmlFor="email" className="block text-lg font-medium text-gray-700">Email</label>
-                                <Field type="email" name="email" id="email" className="w-full h-[42px] bg-white shadow-[inset_2px_2px_7px_#000000] rounded-[25px] p-2 pl-4 text-[#313131] focus:outline-none" placeholder="Jhon@gmail.com" required="" />
-                                {errors.email && touched.email && (
-                                    <Alert type="error" message={errors.email} />
-                                )}
-                            </div>
-
-                            <div className="relative">
-                                <label htmlFor="password" className="block text-lg font-medium text-gray-700">Contraseña</label>
                                 <div className="relative">
-                                    <Field
-                                        type={showPassword ? "text" : "password"}
-                                        name="password"
-                                        id="password"
-                                        className="w-full h-[42px] bg-white shadow-[inset_2px_2px_7px_#000000] rounded-[25px] p-2 pl-4 text-[#313131] focus:outline-none"
-                                        placeholder="********"
-                                        required=""
-                                    />
-                                    <button
-                                        type="button"
-                                        className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-700"
-                                        onClick={() => setShowPassword(!showPassword)}
-                                    >
-                                        {showPassword ? <FaEyeSlash /> : <FaEye />}
-                                    </button>
+                                    <label htmlFor="password" className="block text-lg font-medium text-gray-700">Contraseña</label>
+                                    <div className="relative">
+                                        <Field
+                                            type={showPassword ? "text" : "password"}
+                                            name="password"
+                                            id="password"
+                                            className="w-full h-[42px] bg-white shadow-[inset_2px_2px_7px_#000000] rounded-[25px] p-2 pl-4 text-[#313131] focus:outline-none"
+                                            placeholder="********"
+                                            required=""
+                                        />
+                                        <button
+                                            type="button"
+                                            className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-700"
+                                            onClick={() => setShowPassword(!showPassword)}
+                                        >
+                                            {showPassword ? <FaEyeSlash /> : <FaEye />}
+                                        </button>
+                                    </div>
+                                    {errors.password && touched.password && (
+                                        <Alert type="error" message={errors.password} />
+                                    )}
                                 </div>
-                                {errors.password && touched.password && (
-                                    <Alert type="error" message={errors.password} />
-                                )}
-                            </div>
 
-                            <div className="relative">
-                                <label htmlFor="passwordConfirmation" className="block text-lg font-medium text-gray-700">Confirmar Contraseña</label>
                                 <div className="relative">
-                                    <Field
-                                        type={showConfirmPassword ? "text" : "password"}
-                                        name="passwordConfirmation"
-                                        id="passwordConfirmation"
-                                        className="w-full h-[42px] bg-white shadow-[inset_2px_2px_7px_#000000] rounded-[25px] p-2 pl-4 text-[#313131] focus:outline-none"
-                                        placeholder="********"
-                                        required=""
-                                    />
-                                    <button
-                                        type="button"
-                                        className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-700"
-                                        onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-                                    >
-                                        {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
+                                    <label htmlFor="passwordConfirmation" className="block text-lg font-medium text-gray-700">Confirmar Contraseña</label>
+                                    <div className="relative">
+                                        <Field
+                                            type={showConfirmPassword ? "text" : "password"}
+                                            name="passwordConfirmation"
+                                            id="passwordConfirmation"
+                                            className="w-full h-[42px] bg-white shadow-[inset_2px_2px_7px_#000000] rounded-[25px] p-2 pl-4 text-[#313131] focus:outline-none"
+                                            placeholder="********"
+                                            required=""
+                                        />
+                                        <button
+                                            type="button"
+                                            className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-700"
+                                            onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                                        >
+                                            {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
+                                        </button>
+                                    </div>
+                                    {errors.passwordConfirmation && touched.passwordConfirmation && (
+                                        <Alert type="error" message={errors.passwordConfirmation} />
+                                    )}
+                                </div>
+
+                                <div className="flex items-center justify-center my-2">
+                                    <div className="flex-grow border-t-2 border-gray-500"></div>
+                                </div>
+
+                                <div className='flex ml-5 mt-5'>
+
+                                    <div className="relative flex justify-center items-center">
+                                        <button disabled={!isValid} type="submit" className="w-[144px] left-[116px] bg-[#f78707] text-white py-2 px-4 rounded-xl disabled:opacity-50 transition-transform transform hover:scale-110">Registrarme</button>
+                                    </div>
+                                    <button type="button" className="flex items-center justify-center w-full h-[42px] transition-transform transform hover:scale-125 " onClick={loginWithGoogle}>
+                                        <FaGoogle className="text-white-500 md:text-[44px] " /> oogle
                                     </button>
+
                                 </div>
-                                {errors.passwordConfirmation && touched.passwordConfirmation && (
-                                    <Alert type="error" message={errors.passwordConfirmation} />
-                                )}
-                            </div>
 
-                            <div className="flex items-center justify-center my-2">
-                                <div className="flex-grow border-t-2 border-gray-500"></div>
-                            </div>
+                                <p className="text-xs text-center text-gray-500">
+                                    ¿Ya tienes una cuenta?{' '}
+                                    <a href="/login" className="font-medium text-[#eaefe4] hover:underline">
+                                        Inicia sesión aquí
+                                    </a>
+                                </p>
 
-                            <div className='flex ml-5 mt-5'>
+                            </Form>
+                        )}
 
-                                <div className="relative flex justify-center items-center">
-                                    <button disabled={!isValid} type="submit" className="w-[144px] left-[116px] bg-[#f78707] text-white py-2 px-4 rounded-xl disabled:opacity-50 transition-transform transform hover:scale-110">Registrarme</button>
-                                </div>
-                                <button type="button" className="flex items-center justify-center w-full h-[42px] transition-transform transform hover:scale-125 " onClick={loginWithGoogle}>
-                                    <FaGoogle className="text-white-500 md:text-[44px] " /> oogle
-                                </button>
-
-                            </div>
-
-                            <p className="text-xs text-center text-gray-500">
-                                ¿Ya tienes una cuenta?{' '}
-                                <a href="/login" className="font-medium text-[#eaefe4] hover:underline">
-                                    Inicia sesión aquí
-                                </a>
-                            </p>
-
-                        </Form>
-                    )}
-
-                </Formik>
+                    </Formik>
+                </div>
             </div>
         </div>
     )
